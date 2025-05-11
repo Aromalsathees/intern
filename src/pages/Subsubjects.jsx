56b3b35ecfef78dcca8df12dd48c378fc0bbe8af
Aromal <aromalsatheesh802003@gmail.com>
2025-05-11 20:24:11 +0700

    changes headings

--- src/pages/Subsubjects.jsx
+++ src/pages/Subsubjects.jsx
@@ -23,8 +23,8 @@ const Subsubjects = () => {
   return (
     
   <div>   
-<h1 className="text-3xl font-bold lg:ml-160 ml-30 text-black lg:mb-8 mb-4 lg:mt-30 mt-40">Topics</h1>
-<p className='lg:ml-160 ml-26 lg:mt-[-20px] text-gray-500 text-sm font-bold'>Browse study materials by Topics</p>
+<h1 className="text-3xl font-bold lg:ml-160 ml-22 text-black lg:mb-8 mb-4 lg:mt-30 mt-40">Topics</h1>
+<p className='lg:ml-160 ml-19 lg:mt-[-20px] text-gray-500 text-sm font-bold'>Browse study materials by Topics</p>
 
 <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:ml-20 ml-7 justify-center lg:gap-6 gap-3 lg:mt-10 mt-4  ">
         {cards.map((card, index) => (
