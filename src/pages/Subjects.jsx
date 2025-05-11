f1f1777003a3470db32583ac8d8359cd9061c65b
Aromal <aromalsatheesh802003@gmail.com>
2025-05-11 20:18:50 +0700

    changes

--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -18,8 +18,8 @@ const Subjects = () => {
   return (
     <div>
   
-<h1 className="text-3xl font-bold lg:ml-160 ml-30 text-black lg:mb-8 mb-4 lg:mt-30 mt-40">Subjects</h1>
-<p className='lg:ml-160 ml-26 lg:mt-[-20px] text-gray-500 text-sm font-bold'>Browse study materials by subject</p>
+<h1 className="text-3xl font-bold lg:ml-160 ml-25 text-black lg:mb-8 mb-4 lg:mt-30 mt-40">Subjects</h1>
+<p className='lg:ml-160 ml-20 lg:mt-[-20px] text-gray-500 text-sm font-bold'>Browse study materials by subject</p>
 
 <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:ml-20 ml-7 justify-center lg:gap-6 gap-3 lg:mt-10 mt-4  ">   
         {cards.map((card, index) => (
