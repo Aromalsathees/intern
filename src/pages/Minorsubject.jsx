8cc0469d4932f013f5e049eb0edd7d44c6df0be8
Aromal <aromalsatheesh802003@gmail.com>
2025-05-12 11:48:41 +0700

    changed cards button

--- src/pages/Minorsubject.jsx
+++ src/pages/Minorsubject.jsx
@@ -58,7 +58,7 @@ search bar ends here */}
 
 {/* 
 {/* Cards start here */}
-
+{/* 
  <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:mt-4 mt-8 lg:ml-50 ml-10 gap-6 lg:mr-20 mr-10  '>
  {popular.map((card,index)=>(
      <div key={index} className='border px-4 py-2 lg:w-70 lg:h-40 rounded-lg shadow-md'>
@@ -69,7 +69,22 @@ search bar ends here */}
     </Link>
    </div>
 ))}
- </div>
+ </div> */}
+
+ <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:mt-4 mt-8 lg:ml-50 ml-10 gap-6 lg:mr-20 mr-10'>
+  {popular.map((card, index) => (
+    <div key={index} className='border px-4 py-2 lg:w-70 lg:h-40 rounded-lg shadow-md flex flex-col justify-between'>
+      <div>
+        <p className='font-semibold lg:mt-2 mt-4'>{card.name}</p>
+        <p className='text-gray-400'>Note-2022</p>
+      </div>
+      <Link to={`/materialresorses/${card.id}?model=popular`}>
+        <button className='bg-blue-300 mt-2 rounded-lg text-white w-full'>Download</button>
+      </Link>
+    </div>
+  ))}
+</div>
+
 
 {/* 
 cards ends here
